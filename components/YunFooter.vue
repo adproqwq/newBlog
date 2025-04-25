b2527634516810d441873f9c4cc20ab8f29aaa20
Adpro <101449500+adproqwq@users.noreply.github.com>
2025-04-25 20:09:32 +0800

    docs: 更换友链链接

--- components/YunFooter.vue
+++ components/YunFooter.vue
@@ -9,8 +9,11 @@ const { site } = useAddonVercount();
   <YunFooter>
     <!-- 自定义页脚内容 -->
      <div>
-      <span>本站总访问量 {{ site.pv }} 次</span>
-      <span>本站总访客数 {{ site.uv }} 人</span>
+      <span>
+        本站总访问量 {{ site.pv }} 次
+
+        本站总访客数 {{ site.uv }} 人
+      </span>
      </div>
     <div>
       <a href="https://icp.gov.moe/?keyword=20233825" target="_blank">萌ICP备20233825号</a>

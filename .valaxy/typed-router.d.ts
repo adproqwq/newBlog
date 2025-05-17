0b01287486e942e7a54cae38992aa7e12d08560f
Adpro <101449500+adproqwq@users.noreply.github.com>
2025-05-17 20:17:36 +0800

    docs: new post

--- .valaxy/typed-router.d.ts
+++ .valaxy/typed-router.d.ts
@@ -30,6 +30,7 @@ declare module 'vue-router/auto-routes' {
     '/posts/': RouteRecordInfo<'/posts/', '/posts', Record<never, never>, Record<never, never>>,
     '/posts/关于本人GKD订阅终止维护的通知': RouteRecordInfo<'/posts/关于本人GKD订阅终止维护的通知', '/posts/关于本人GKD订阅终止维护的通知', Record<never, never>, Record<never, never>>,
     '/posts/随笔': RouteRecordInfo<'/posts/随笔', '/posts/随笔', Record<never, never>, Record<never, never>>,
+    '/posts/哲学的基本问题和两大派别': RouteRecordInfo<'/posts/哲学的基本问题和两大派别', '/posts/哲学的基本问题和两大派别', Record<never, never>, Record<never, never>>,
     '/posts/GKD本地订阅收录申请': RouteRecordInfo<'/posts/GKD本地订阅收录申请', '/posts/GKD本地订阅收录申请', Record<never, never>, Record<never, never>>,
     '/posts/GKD规则编写之属性-1': RouteRecordInfo<'/posts/GKD规则编写之属性-1', '/posts/GKD规则编写之属性-1', Record<never, never>, Record<never, never>>,
     '/posts/GKD规则编写之属性-2': RouteRecordInfo<'/posts/GKD规则编写之属性-2', '/posts/GKD规则编写之属性-2', Record<never, never>, Record<never, never>>,

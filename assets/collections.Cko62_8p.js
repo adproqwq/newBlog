11bb055cad86f433b495bd166aca06af333b241d
adproqwq <adproqwq@users.noreply.github.com>
2026-01-18 06:10:32 +0000

    deploy: 850af14c52eee95184ec1ebf6476870039ad4602

---
+++ assets/collections.Cko62_8p.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.DqWGeG26.js";import{u as l}from"./chunks/vue-router.B_NUezYe.js";import{A as t}from"./framework.Dh18Cujy.js";const e=[];function a(){return{collections:t(()=>e)}}function p(){const o=l(),n=t(()=>o.path.startsWith("/collections/")?o.path.split("/")[2]:"");return{collection:t(()=>e.find(c=>c.key===n.value))}}export{a,p as u};

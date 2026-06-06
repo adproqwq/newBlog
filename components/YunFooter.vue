446549cecb95ae6d47cdba3f167f49a454c1e2cd
風間青祢 <101449500+adproqwq@users.noreply.github.com>
2026-06-07 00:39:13 +0800

    perf: remove invalid footer links

--- components/YunFooter.vue
+++ components/YunFooter.vue
@@ -1,17 +1,8 @@
 <script lang="ts" setup>
 import { useAddonVercount } from 'valaxy-addon-vercount';
 import YunFooter from 'valaxy-theme-yun/components/YunFooter.vue';
-import { onMounted } from 'vue';
 
 const { site } = useAddonVercount();
-
-onMounted(() => {
-  const wuIcpCss = document.createElement('link');
-  wuIcpCss.rel = 'stylesheet';
-  wuIcpCss.href = 'https://scdn.星.fun/icp.css';
-
-  document.head.append(wuIcpCss);
-});
 </script>
 
 <template>
@@ -27,32 +18,10 @@ onMounted(() => {
     <div class="icp">
       <a href="https://icp.gov.moe/?keyword=20233825" target="_blank">萌ICP备20233825号</a>
 
-      <a href="https://icp.xnet.ren/id.php?keyword=20250825" target="_blank">
-        <img src="https://icp.xnet.ren/favicon.ico" width="20" height="20" style="vertical-align: middle; margin-right: 5px; display: inherit;">
-        信备20250825号
-      </a>
-
-      <a href="https://icp.星.fun/id.php?keyword=20250825" target="_blank">
-        <img src="https://icp.星.fun/logo.png" width="20" height="20" style="vertical-align: middle; margin-right: 5px; display: inherit;">
-        团ICP备20250825号
-      </a>
-
-      <a href="https://icp.govs.wang/id.php?keyword=20250825" target="_blank">
-        <img src="https://icp.govs.wang/favicon.ico" width="20" height="20" style="vertical-align: middle; margin-right: 5px; display: inherit;">
-        网ICP备20250825号
-      </a>
-
       <a href="https://icp.redcha.cn/beian/ICP-2025080071.html">
         <img src="https://icp.redcha.cn/static/picture/icplogoi.png" width="20" height="20" style="vertical-align: middle; margin-right: 5px; display: inherit;">
         茶ICP备2025080071号
       </a>
-
-      <div class="github-badge">
-        <span class="badge-subject">雾备</span>
-        <a style="color:#fff" href="https://icp.wudu.ltd/id.php?keyword=20080825" rel="external" target="_blank">
-          <span class="badge-value bg-cai">20080825号</span>
-        </a>
-      </div>
     </div>
   </YunFooter>
 </template>
